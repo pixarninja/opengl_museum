cf866fdbd1ed938a071e6bde368c0eea3fc1f6d4
pixarninja <wesharris505@gmail.com>
2018-01-18 15:03:57 -0700

    Main file is a bit better now

---
+++ CMakeFiles/window_executable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/window_executable.dir/main.cpp.o"
+  "window_executable.pdb"
+  "window_executable"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/window_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
