dd83188fb60ddf6911315ed95ef0a81bbe6e8c14
pixarninja <wesharris505@gmail.com>
2018-03-06 16:41:55 -0700

    Complete object loader for deadline on 3/7/18

--- setup_environment/Glitter/Headers/Object.h
+++ setup_environment/Glitter/Headers/Object.h
@@ -1,13 +1,17 @@
 /******************************************************************************
-	Name: Amy Knowles
+	Name: Wes and Zach Harris
 	File Name: Object.h
 	Synopsis: This file contains variable and function declarations for the 
 			  Object class.
 ******************************************************************************/
 
 #pragma once
+#ifndef MODEL_VIEW_OBJECT_H
+#define MODEL_VIEW_OBJECT_H
+
+#endif //MODEL_VIEW_OBJECT_H
+
 #include <string>
-//#include <GL/glew.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -16,7 +20,7 @@
 #include <glm/vec3.hpp>	// glm::vec3
 #include <glm/vec4.hpp>	// glm::vec4
 #include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/gtc/matrix_transform.hpp>	
+#include <glm/gtc/matrix_transform.hpp>
 // glm::translate, glm::rotate, glm::scale, glm::perspective
 
 class Object
--- setup_environment/Glitter/Sources/main.cpp
+++ setup_environment/Glitter/Sources/main.cpp
@@ -1,50 +1,248 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+/******************************************************************************
+	Name: Wes and Zach Harris
+	File Name: model_view.cpp
+	Synopsis: This file includes an object class that will load a simple model
+              from an object file, as well as a main driver that will render
+              the loaded models onto the viewing window.  You will also be
+              able to dynamically switch between your simple loaded objects.
+******************************************************************************/
 
-#include <iostream>
-#include <Object.h>
+#include "Object.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+/******************************************************************************
 
-//settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+	Shader Set-up
 
-const char *vertexShaderSource = "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
+******************************************************************************/
+
+const char *vertexShaderSource = "#version 330\n"
+        "in vec4 vPosition;\n"
+        "in vec4 vColor;\n"
+        "out vec4 color;\n"
+        "uniform vec3 theta;\n"
         "void main()\n"
         "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   //Compute the sines and cosines of theta for each of\n"
+        "   //the three axes in one computation.\n"
+        "   vec3 angles = radians(theta);\n"
+        "   vec3 c = cos(angles);\n"
+        "   vec3 s = sin(angles);\n"
+        "   //Remember: these matrices are column major.\n"
+        "   mat4 rx = mat4(1.0, 0.0, 0.0, 0.0,\n"
+        "                  0.0, c.x, s.x, 0.0,\n"
+        "                  0.0, -s.x, c.x, 0.0,\n"
+        "                  0.0, 0.0, 0.0, 1.0);\n"
+        "   mat4 ry = mat4(c.y, 0.0, -s.y, 0.0,\n"
+        "                  0.0, 1.0, 0.0, 0.0,\n"
+        "                  s.y, 0.0, c.y, 0.0,\n"
+        "                  0.0, 0.0, 0.0, 1.0);\n"
+        "   mat4 rz = mat4(c.z, -s.z, 0.0, 0.0,\n"
+        "                  s.z, c.z, 0.0, 0.0,\n"
+        "                  0.0, 0.0, 1.0, 0.0,\n"
+        "                  0.0, 0.0, 0.0, 1.0);\n"
+        "   color = vColor * 1.5;\n"
+        "   gl_Position = rz * ry * rx * vPosition;\n"
         "}\0";
 
-const char *fragmentShaderSource = "#version 330 core\n"
-        "out vec4 FragColor;\n"
+const char *fragmentShaderSource = "#version 330\n"
+        "in vec4 color;\n"
+        "out vec4 fColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "   fColor = color;\n"
         "}\n\0";
 
+/******************************************************************************
 
+	GLOBAL VARIABLES
+
+******************************************************************************/
+
+// Array of rotation angles (degrees) for each coordinate axis
+enum { Xaxis = 0, Yaxis = 1, Zaxis = 2, NumAxes = 3 };
+int Axis = Xaxis;
+
+const int NUM_MODELS = 2;
+
+GLfloat Theta[NumAxes] = { 0.0, 0.0, 0.0 };
+Object objects[NUM_MODELS] = {
+        std::string("/home/pixarninja/Git/opengl_museum/setup_environment/Glitter/Sources/cube.obj"),
+        std::string("/home/pixarninja/Git/opengl_museum/setup_environment/Glitter/Sources/first_cut.obj")
+};
+GLuint vao[NUM_MODELS];
+int modelSelection = 0;
+
+// the location of the "theta" shader uniform variable
+GLint theta;
+
+// set up our window size
+const unsigned int SCR_WIDTH = 700;
+const unsigned int SCR_HEIGHT = 700;
+
+/******************************************************************************
+
+	OPENGL INITIALIZATION
+
+******************************************************************************/
+void init() {
+    // create a vertex array object (vao)
+    glGenVertexArrays(NUM_MODELS, vao);
+
+    // Build and compile our shader program
+    // ---------- Vertex Shader -----------
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // ---------- Fragment Shader ----------
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // Link the shaders to the program
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    // check for linking errors
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success){
+        glGetProgramInfoLog(program, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED.\n"
+                  << infoLog << std::endl;
+    }
+
+    // now lets get rid of the shaders since
+    // we have linked them to the program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // now let's link all this stuff up to our VAO
+    // and prep to display our objects
+    glUseProgram(program);
+    for(int i = 0; i < NUM_MODELS; i++){
+        glBindVertexArray(vao[i]);
+        objects[i].load(program);
+    }
+
+    glBindVertexArray(vao[0]);
+    theta = glGetUniformLocation(program, "theta");
+
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(1.0, 1.0, 1.0,1.0);
+
+} // end init()
+
+/******************************************************************************
+
+	DISPLAY LOADED OBJECT
+
+*******************************************************************************/
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUniform3fv(theta, 1, Theta);
+
+    // draw the currently loaded model
+    objects[modelSelection].draw();
+} // end display()
+
+/******************************************************************************
+
+	KEYBOARD SWITCH COMMANDS
+
+******************************************************************************/
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        exit(EXIT_SUCCESS);
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
+        modelSelection = (modelSelection + 1) % NUM_MODELS;
+        glBindVertexArray(vao[modelSelection]);
+    }
+} // end key_callback()
+
+/******************************************************************************
+
+	MOUSE COMMANDS
+
+******************************************************************************/
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+        Axis = Zaxis;
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        Axis = Xaxis;
+    if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS)
+        Axis = Yaxis;
+} // end mouse_button_callback()
+
+/******************************************************************************
+
+	AXIS ROTATION
+
+******************************************************************************/
+void idle() {
+    Theta[Axis] += 1.0; // might try using 0.01 intervals
+
+    if (Theta[Axis] > 360.0) {
+        Theta[Axis] -= 360.0;
+    }
+} // end idle()
+
+/******************************************************************************
+
+	MAIN
+
+******************************************************************************/
 int main(){
 
     // glfw: initialize and configure
-    // ------------------------------
+    // -----------------------------------------------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(500 /*SCR_WIDTH*/, 500 /*SCR_HEIGHT*/, "Harris_Harris Object Loader", nullptr, nullptr);
+    // -----------------------------------------------------------------------
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // create a windowed window ... something that is not
+    // full screen
+    // -----------------------------------------------------------------------
+    GLFWwindow* window =
+            glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,
+                             "Harris_Harris Object Viewer",
+                             nullptr, nullptr
+            );
     if(window == nullptr){
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
+    // make the OpenGL context active
+    // -----------------------------------------------------------------------
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -53,114 +251,28 @@ int main(){
         return -1;
     }
 
-    // build and compile our shader program
-    // ------------------------------------
-    // vertex shader
-    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // fragment shader
-    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // link shaders
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    // call init()
+    // -----------------------------------------------------------------------
+    init();
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    GLfloat *vertices;
-    Object object("/home/pixarninja/Git/opengl_museum/setup_environment/Glitter/Sources/cube.obj");
-    object.getObjInfo();
-    int numVertices = object.initVertArray(&vertices);
-    std::cout << numVertices << std::endl;
+    // set up the requirements for our key/mouse clicks
+    // -----------------------------------------------------------------------
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
 
-    std::cout << "vertices[] = {" << std::endl;
-    for(int i = 0; i < numVertices * 3; i += 3) {
-        std::cout << i / 3 << ": " << vertices[i] << ", " << vertices[i + 1] << ", " << vertices[i + 2] << std::endl;
-    }
-    std::cout << "}" << std::endl;
-
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    //glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(GLfloat[3]), vertices, GL_STATIC_DRAW);
-    object.load(shaderProgram);
-
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    // render loop
-    // -----------
+    // glfw uses a "closed event" loop, which means you only have to handle
+    // events when you need to
+    // -----------------------------------------------------------------------
     while(!glfwWindowShouldClose(window)){
-
-        // input
-        // -----
-        processInput(window);
-
-        // render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // draw our first triangle
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        object.draw();
-        //glDrawArrays(GL_TRIANGLES, 0, numVertices);
-        // glBindVertexArray(0); // no need to unbind it every time
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
+        //retrieve window events
         glfwSwapBuffers(window);
+        display();
+        idle();
         glfwPollEvents();
     }
 
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-
     // glfw: terminate, clearing all previously allocated GLFW resources
-    // -----------------------------------------------------------------
+    // -----------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
